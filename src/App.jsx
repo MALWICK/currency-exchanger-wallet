51b0d464f04ad1cfec55dcade9ef605bceaef891
MALWICK <batahumphrey66@gamil.com>
2023-07-06 15:34:01 +0100

    crwated conversion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import CurrencyExchanger from './Context/Context';
@@ -10,6 +10,38 @@ function App() {
   const [currency1, setCurrency1] = useState(100);
   const [currency2, setCurrency2] = useState(500);
   const [currency3, setCurrency3] = useState(10000);
+  const [totalCurrency, setTotalCurrency] = useState(0);
+  const [select, setSelect] = useState();
+
+  useEffect(() => {
+    if (select === undefined) {
+      const defaultCurrency = `${(
+        currency2 * 1.07 +
+        currency3 * 0.0016 +
+        currency1
+      ).toFixed(2)} USD`;
+      setTotalCurrency(defaultCurrency);
+    } else if (select === 'USD') {
+      const amount2 = currency2 * 1.07;
+      const amount3 = currency3 * 0.0016;
+      const total = `${(amount2 + amount3 + currency1).toFixed(2)} USD`;
+      setTotalCurrency(total);
+    } else if (select === 'EUR') {
+      const amount1 = currency1 * 0.93;
+      const amount3 = currency3 * 0.0015;
+      const total = `${(amount1 + amount3 + currency2).toFixed(2)} EUR`;
+      setTotalCurrency(total);
+    } else {
+      const amount1 = currency1 * 610.76;
+      const amount2 = currency2 * 655.76;
+      const total = `${(amount1 + amount2 + currency3).toFixed(2)} XAF`;
+      setTotalCurrency(total);
+    }
+  });
+
+  const handelSelect = (e) => {
+    setSelect(e.target.value);
+  };
   return (
     <div className="App">
       <CurrencyExchanger.Provider
@@ -20,6 +52,9 @@ function App() {
           currency1,
           currency2,
           currency3,
+          setTotalCurrency,
+          totalCurrency,
+          handelSelect,
         }}
       >
         <BrowserRouter>
--- src/components/WalletOpera/WalletOPeration.jsx
+++ src/components/WalletOpera/WalletOPeration.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './walletOperation.css';
 import DepositBtn from '../Button/DepositBtn/DepositBtn';
 import Transfer from '../Button/TranferBtn/Transfer';
+import CurrencyExchanger from '../../Context/Context';
 
 function WalletOPeration() {
+  const { currency1, currency2, currency3, totalCurrency, handelSelect } =
+    useContext(CurrencyExchanger);
   return (
     <div className="wallet__operation">
       <h1>International business payments</h1>
       <div className="wallet__operationContainer">
         <div className="operation__totalAmt">
           <p>Wallet Value</p>
-          <span>0.00</span>
+          <span>{totalCurrency}</span>
         </div>
         <div className="multi__task">
           <DepositBtn />
           <Transfer />
           <div className="select__initial_value">
             <p>select Default currency</p>
-            <select id="select__default" className="select__defult">
-              <option value="usd">usd</option>
-              <option value="eru">eru</option>
-              <option value="xfa">Xfa</option>
+            <select
+              id="select__default"
+              onChange={handelSelect}
+              className="select__defult"
+            >
+              <option value="USD">usd</option>
+              <option value="EUR">eru</option>
+              <option value="XAF">Xfa</option>
             </select>
           </div>
         </div>
@@ -28,19 +35,19 @@ function WalletOPeration() {
           <div className="wallet__one">
             <div className="wallet__oneInner">
               <p>USD</p>
-              <p>0.00</p>
+              <p>{currency1}</p>
             </div>
           </div>
           <div className="wallet__one">
             <div className="wallet__oneInner">
               <p>Eru</p>
-              <p>0.00</p>
+              <p>{currency2}</p>
             </div>
           </div>
           <div className="wallet__one">
             <div className="wallet__oneInner">
               <p>XFA</p>
-              <p>0.00</p>
+              <p>{currency3}</p>
             </div>
           </div>
         </div>
