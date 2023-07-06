c566f8eb2525d77bcf4dd576d8991dbe84f11514
MALWICK <batahumphrey66@gamil.com>
2023-07-06 13:26:06 +0100

    created the main convertion ui

--- src/components/WalletOpera/WalletOPeration.jsx
+++ src/components/WalletOpera/WalletOPeration.jsx
@@ -3,8 +3,36 @@ import './walletOperation.css';
 
 function WalletOPeration() {
   return (
-    <div>
-      <p>WalletOPeration</p>
+    <div className="wallet__operation">
+      <h1>The fast and trusted way to send money</h1>
+      <div className="wallet__operationContainer">
+        <div className="operation__totalAmt">
+          <p>Wallet Value</p>
+          <p>0.00</p>
+        </div>
+        <div className="multi__task">
+          <p>Deposit</p>
+          <p>Transfer Money</p>
+          <div className="select__initial_value">
+            <select id="select__default" className="select__defult">
+              <option value="usd">usd</option>
+              <option value="eru">eru</option>
+              <option value="xfa">Xfa</option>
+            </select>
+          </div>
+        </div>
+        <div className="Wallets__holder">
+          <div className="wallet__one">
+            <p>usd</p>
+          </div>
+          <div className="wallet__two">
+            <p>euro</p>
+          </div>
+          <div className="wallet__three">
+            <p>Xfa</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Accounts/Accounts.jsx
+++ src/pages/Accounts/Accounts.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import './account.css';
 import WalletNavbar from '../../components/WalletNavBar/WalletNavbar';
 import WalletConta1 from '../../components/WalletCont1/WalletConta1';
+import WalletOPeration from '../../components/WalletOpera/WalletOPeration';
 
 function Accounts() {
   return (
-    <div>
+    <div className="accounts">
       <WalletNavbar />
       <WalletConta1 />
+      <WalletOPeration />
     </div>
   );
 }
